127547e45c2da7a4f5fee3904ea9ed85ad58713c
Davide <davidegalbiati29@gmail.com>
2024-07-17 10:52:06 +0200

    Added the ordering by date in admin page

--- dashboard_arkadia_v2/funds_and_strategies/admin.py
+++ dashboard_arkadia_v2/funds_and_strategies/admin.py
@@ -9,36 +9,43 @@ class StrategyAdmin(admin.ModelAdmin):
     list_display = ('name', 'fund', 'manual', 'description')
     search_fields = ('name', 'fund__name')
     list_filter = ('fund',)
+    ordering = ('name',)
 
 class AssetAdmin(admin.ModelAdmin):
     list_display = ('name', 'strategy', 'exchange_account', 'wallet', 'amount', 'value_usd', 'date')
     search_fields = ('name', 'strategy__name')
-    list_filter = ('strategy',)
+    list_filter = ('strategy', 'date')
+    ordering = ('date',)
 
 class BalanceAdmin(admin.ModelAdmin):
     list_display = ('strategy', 'value_usd', 'date')
     search_fields = ('strategy__name',)
     list_filter = ('strategy', 'date')
+    ordering = ('date',)
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('type', 'asset', 'amount', 'value_usd', 'date', 'strategy')
     search_fields = ('type', 'strategy__name')
-    list_filter = ('type', 'strategy')
+    list_filter = ('type', 'strategy', 'date')
+    ordering = ('date',)
 
 class PerformanceMetricAdmin(admin.ModelAdmin):
     list_display = ('strategy', 'date', 'metric_name', 'value')
     search_fields = ('strategy__name', 'metric_name')
     list_filter = ('strategy', 'date', 'metric_name')
+    ordering = ('date',)
 
 class ExchangeAccountAdmin(admin.ModelAdmin):
     list_display = ('name', 'strategy', 'exchange')
     search_fields = ('name', 'strategy__name')
     list_filter = ('strategy',)
+    ordering = ('name',)
 
 class WalletAdmin(admin.ModelAdmin):
     list_display = ('name', 'strategy', 'address', 'network', 'description', 'last_updated')
     search_fields = ('name', 'strategy__name')
     list_filter = ('strategy',)
+    ordering = ('name',)
 
 admin.site.register(Fund, FundAdmin)
 admin.site.register(Strategy, StrategyAdmin)
