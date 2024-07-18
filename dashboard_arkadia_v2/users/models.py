04c326f23e58043592d546fd936e98c0319d7d05
Davide <davidegalbiati29@gmail.com>
2024-07-18 11:38:19 +0200

    Implemented the management of users

--- dashboard_arkadia_v2/users/models.py
+++ dashboard_arkadia_v2/users/models.py
@@ -1,20 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
+from funds_and_strategies.models import Fund
 
 class CustomUser(AbstractUser):
     is_privileged = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='customuser_set',  # Nome unico per evitare conflitti
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        related_query_name='customuser',
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='customuser_set',  # Nome unico per evitare conflitti
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_query_name='customuser',
-    )
+    def __str__(self):
+        return self.username
